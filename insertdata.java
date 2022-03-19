b94e7ffd9d1d3014dd538f20f48035c838aa59d0
EnidFerns <69514959+EnidFerns@users.noreply.github.com>
2022-03-19 23:17:27 +0530

    Add files via upload

---
+++ insertdata.java
@@ -0,0 +1,54 @@
+package net.codejava;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class insertdata {
+
+   
+    private Connection connect() {
+        // SQLite connection string
+		String jdbcUrl = "jdbc:sqlite:/C:\\SQLite\\sqlite-tools-win32-x86-3380100\\movies.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(jdbcUrl);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    
+    public void insert (String movie_name,String lead_actor,String lead_actress, Integer release_year,String director) {
+        String sql = "INSERT INTO moviedetails (Movie_Name,Lead_Actor,Lead_Actress,release_year, director) VALUES(?,?,?,?,?)";
+
+        try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            	pstmt.setString(1, movie_name);
+            	pstmt.setString(2, lead_actor);
+            	pstmt.setString(3, lead_actress);
+		
+            	pstmt.setInt(4, release_year);
+            	pstmt.setString(5, director);
+            	pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        insertdata data = new insertdata();
+        
+        data.insert("3 Idiots","Amir Khan","Kareena Kapoor",2009, "Rajkumar Hirani");
+        data.insert("Chhichhore","Sushant Singh Rajput","Shraddha Kapoor",2019, "Nitesh Tiwari");
+	
+    }
+
+
+}
+
